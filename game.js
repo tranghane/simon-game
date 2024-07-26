e76d10c4123f8ab6ed8ef90802548cdd3fe5c38f
Your Name <trungduongnguyen2004@gmail.com>
2024-07-25 21:58:19 -0700

    finishing the game

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
 var randomChosenColour;
 var gamePattern = [];
+var userClickedPattern = [];
 const buttonColours = ["red", "blue", "green", "yellow"];
 var level = 0;
 var randomNumber = -1;
+
+//Starting the game:
+var started = false;
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 function nextSequence() {
+  //reset userPattern:
+  userClickedPattern = [];
   randomNumber = Math.floor(Math.random() * 4);
   if (randomNumber == 4) {
     randomNumber = 3;
@@ -21,21 +35,64 @@ function nextSequence() {
 
   //play the color sound
   playSound(randomChosenColour);
+
+  //increase level:
+  $("#level-title").text("Level " + level);
+  level++;
 }
+$(document).on("click", ".btn", function () {
+  // Get the ID of the clicked button
+  var userChosenColour = this.id;
+  console.log(userChosenColour); //test
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  // You can perform further actions based on the userChosenColour here
 
+  checkAnswer(userClickedPattern.length - 1);
+});
 
+function checkAnswer(curIndex) {
+  if (userClickedPattern[curIndex] === gamePattern[curIndex]) {
+    //if chosen the correct one, keep going, until the last color:
+    if (userClickedPattern.length === gamePattern.length) {
+      //go to next sequence
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // wrong, do again
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
 
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
 
 console.log(randomChosenColour);
 console.log(gamePattern);
 function playSound(name) {
-  var audio = new Audio("sounds/wrong.mp3");
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
-// var audio = new Audio("sounds/wrong.mp3");
-// audio.play();
-// document.addEventListener('click', function() {
-//     var audio = new Audio("sounds/wrong.mp3");
-//     audio.play();
-//   });
-  
+function animatePress(currentColor) {
+  //add class pressed
+  $("#" + currentColor).addClass("pressed");
+  //remove class pressed after 1 second
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+//reset every value and start over
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
